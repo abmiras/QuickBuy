43fd2258f00f597a30f86c0d9de6e12e6c2d9889
Abner Miras <amiras@gmail.com>
2020-09-22 11:38:50 -0300

    Criação das Entidades

---
+++ QuickBuy.Dominio/Entidades/Entidade.cs
@@ -0,0 +1,15 @@
+﻿using System.Dynamic;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class Entidade
+    {
+        public int Id {get;set;}
+
+        public string Email { get; set; }
+
+        public string Senha { get; set; }
+
+
+    }
+}

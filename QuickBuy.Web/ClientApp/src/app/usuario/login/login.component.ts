f713e43a6d42668cdae50d1e53db608a99a60318
Abner Miras <amiras@gmail.com>
2020-10-01 15:11:04 -0300

    .

--- QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
+++ QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
@@ -1,1 +1,18 @@
-﻿
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"]
+
+})
+export class LoginComponent {
+
+  public email = "";
+  public senha = "";
+
+  entrar() {
+    alert(this.email + this.senha);
+  }
+
+}
